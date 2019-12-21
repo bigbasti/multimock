4c1617f58f387a6b416d46acf6c95a97b6dd44ef
Sebastian Gross <s.gross@t-systems.com>
2019-12-21 14:47:48 +0100

    Reverted logic to start a watcher process - now ProcessService handles everything

--- bpmn/src/main/java/com/multimock/bpmn/watcher/DirectoryWatcher.java
+++ bpmn/src/main/java/com/multimock/bpmn/watcher/DirectoryWatcher.java
@@ -1,10 +1,7 @@
 package com.multimock.bpmn.watcher;
 
-import com.multimock.bpmn.service.ProcessService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
